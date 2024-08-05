13607d8a76cf458439a53940c1038471b1dc3517
DA6IjY6jgHT8Z <45499955+DA6IjY6jgHT8Z@users.noreply.github.com>
2024-08-05 10:12:16 -0400

    add lambda code

---
+++ get_caller_id_lambda.py
@@ -0,0 +1,35 @@
+import json
+import logging
+import boto3
+     
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('caller_id')
+
+    
+def read_column(key, column):
+    response_col = "None"
+    try:
+        response = table.get_item(
+            Key={
+                'caller_phone':key
+            }
+        )
+        logger.info(response)
+        response_col = response["Item"][column]
+    except Exception as e:
+        logger.error("DDB Read Error")
+        logger.error(e)
+    return response_col
+
+def lambda_handler(event, context):
+    logger.info(json.dumps(event, indent=2))
+    caller_phone = event["Details"]["ContactData"]["CustomerEndpoint"]["Address"]
+    response = read_column(caller_phone, "sales_rep")
+
+    return {
+        "sales_rep": response
+    }
+
---
+++ holiday_lambda.py
@@ -0,0 +1,53 @@
+import json
+import logging
+import boto3
+
+import datetime
+import dateutil.tz
+     
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('holidays')
+
+    
+def read_column(date, column):
+    response_col = "None"
+    try:
+        response = table.get_item(
+            Key={
+                'date':date
+            }
+        )
+        logger.info(response)
+        response_col = response["Item"][column]
+    except Exception as e:
+        logger.error("DDB Read Error")
+        logger.error(e)
+    return response_col
+
+def lambda_handler(event, context):
+    logger.info(json.dumps(event, indent=2))
+    
+    eastern = dateutil.tz.gettz('US/Eastern')
+    dt_us_eastern = datetime.datetime.now(tz=eastern)
+    logger.info (dt_us_eastern)
+    iso_date = datetime.date.isoformat(dt_us_eastern)
+    str_date = dt_us_eastern.strftime("%Y-%m-%d")
+
+    logger.info("The attributes of now() are : ")
+      
+    logger.info (dt_us_eastern.year)
+    logger.info (dt_us_eastern.month)
+    logger.info (dt_us_eastern.day)
+
+    logger.info(str_date)
+
+    str_date = "2024-12-25"
+    response = read_column(str_date, "description")
+
+    return {
+        "holiday": response
+    }
+
